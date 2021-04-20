52b0c7d158781d9f8900720cd110a3e7078a6ac6
isuly <isulyshka@mail.ru>
2021-04-20 10:40:23 +0300

    add endpoints

--- app/main.py
+++ app/main.py
@@ -6,7 +6,20 @@ auth_app = FastAPI()
 
 @auth_app.get("/")
 async def root():
+    """Initial endpoint"""
     return {"message": "Hello World"}
+
+
+@auth_app.get("/get_code")
+async def get_auth_code():
+    """Get code for authorization"""
+    pass
+
+
+@auth_app.get("/auth")
+async def auth():
+    """Authorization by phone and code"""
+    pass
 
 
 def run_auth_app():
